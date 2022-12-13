84ce8562de68de439cfa81a77c829c0495743550
mdemon420 <mdeamon4@gmail.com>
2022-12-14 01:58:46 +0600

    photo set

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -6,7 +6,18 @@ import { demoProfilePicture } from "../utils/constants";
 import { height } from "@mui/system";
 const ChannelCard = ({ channelDetail }) => {
   return (
-    <Box sx={{ boxShadow: "none", borderRadius: "20px" }}>
+    <Box
+      sx={{
+        boxShadow: "none",
+        borderRadius: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: { xs: "256px", md: "320px" },
+        height: "326px",
+        margin: "auto",
+      }}
+    >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
         <CardContent
           sx={{
@@ -30,7 +41,27 @@ const ChannelCard = ({ channelDetail }) => {
               mb: 2,
               border: "1px solid #e3e3e3",
             }}
-          ></CardMedia>
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle
+              sx={{
+                fontSize: 14,
+                color: "gray",
+                ml: "5px",
+              }}
+            />
+          </Typography>
+          {channelDetail.statistics?.subscriberCount && (
+            <Typography
+              sx={{ fontSize: "15px", fontWeight: 500, color: "gray" }}
+            >
+              {parseInt(
+                channelDetail?.statistics?.subscriberCount
+              ).toLocaleString("en-US")}{" "}
+              Subscribers
+            </Typography>
+          )}
         </CardContent>
       </Link>
     </Box>
