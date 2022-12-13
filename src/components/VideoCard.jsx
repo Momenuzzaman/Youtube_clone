3bba57d02a73437b988bbc4541b38415597750aa
mdemon420 <mdeamon4@gmail.com>
2022-12-14 00:01:56 +0600

    show video

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { typography, Card, CardContent, CardMedia } from "@mui/material";
+import {
+  typography,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 import {
   demoThumbnailUrl,
@@ -8,9 +14,56 @@ import {
   demoChannelUrl,
   demoChannelTitle,
 } from "../utils/constants";
+import { Link } from "react-router-dom";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card
+      sx={{
+        width: { md: "320px", xs: "100%" },
+        boxShadow: "none",
+        borderRadius: "none",
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+      <Card
+        sx={{
+          backgroundColor: "#1e1e1e",
+          height: "106px",
+          borderRadius: "0px",
+        }}
+      >
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" font-weight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle2" font-weight="bold" color="gray">
+            {snippet?.demoChannelTitle || demoChannelTitle}
+            <CheckCircle
+              sx={{
+                fontSize: 13,
+                color: "gray",
+                ml: "5px",
+              }}
+            />
+          </Typography>
+        </Link>
+      </Card>
+    </Card>
+  );
 };
 
 export default VideoCard;
